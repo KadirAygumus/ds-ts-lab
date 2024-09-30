3d501d37bd57b77d1d6dbbaf935baa28b6fe230a
Kadir Aygumus <kadiraygumus@gmail.com>
2024-09-30 22:03:19 +0100

    Complete type-safety to colleagues data

--- src/basics.ts
+++ src/basics.ts
@@ -3,6 +3,19 @@ interface Friend {
     phone: string;
     age: number
 }
+interface Colleague {
+    name: string;
+    department: string;
+    contact: {
+      email: string;
+      extension: number;
+    };
+  }
+interface ColleagueHistory {
+    current: Colleague[],
+    former: Colleague[]
+  }
+  
 
 const friend1 : Friend = {
     name: "Paul Fleming",
@@ -20,7 +33,7 @@ const friend1 : Friend = {
   console.log(friends[1]);
   
   //   -------------------
-  const colleague1 = {
+  const colleague1 : Colleague = {
     name: "Ralph Graham",
     department: "Engineering",
     contact: {
@@ -29,7 +42,7 @@ const friend1 : Friend = {
     },
   };
   
-  const colleague2 = {
+  const colleague2 : Colleague= {
     name: "Patti Burke",
     department: "Finance",
     contact: {
@@ -38,7 +51,7 @@ const friend1 : Friend = {
     },
   };
   
-  const colleague3 = {
+  const colleague3 : Colleague = {
     name: "Dean Sullivan",
     department: "HR",
     contact: {
@@ -46,9 +59,9 @@ const friend1 : Friend = {
       extension: 125,
     },
   };
-  const colleagues = {
+  export const colleagues : ColleagueHistory = {
     current: [colleague1, colleague2, colleague3],
     former: [],
   };
-  
+    
   console.log(colleagues.current[0]);
